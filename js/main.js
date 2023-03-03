f50f29aa9c4c23a4de7a7a5c50ebdc0e17e2f66b
Giuseppe Suanno <suanno.giuseppe@yahoo.it>
2023-03-03 17:28:19 +0100

    Fine esercizio senza bonus

--- js/main.js
+++ js/main.js
@@ -1,19 +1,22 @@
 let grigliaDom = document.getElementById('griglia');
+let button = document.getElementById('btn');
+button.addEventListener('click' , function(){
+    for (let i = 1; i < 101; i++) {
+        const quadrato = creazioneQuadrato();
+        quadrato.addEventListener( 'click', function (){
+            this.classList.toggle('clicked');
+            console.log(i);
+        });
+        quadrato.innerHTML=i;
+        
+        grigliaDom.append(quadrato);
+    }
+})
 
-for (let i = 1; i < 101; i++) {
-    console.log(i);
-    
-    const quadrato = creazioneQuadrato();
-    
-    quadrato.addEventListener( 'click', function (){
-        this.classList.toggle('clicked');
-    });
-
-    grigliaDom.append(quadrato);
-}
 
 function creazioneQuadrato () {
     const nuovoElemento = document.createElement('div');
     nuovoElemento.classList.add('square');
     return nuovoElemento;
+    
 }
